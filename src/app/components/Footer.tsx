248ea90c3aab13c623d74523710a3701a94835bb
Zeeshan Haider <taimoorhassankhan2@gmail.com>
2025-08-17 19:48:40 +0500

    Footer.tsx

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,3 +1,29 @@
+// import Image from "next/image";
+// import Link from "next/link";
+
+// // src/components/Footer.tsx
+// const Footer = () => {
+//   return (
+//     <footer className="bg-[#111827] text-gray-300 py-6 text-center">
+//       <div className="flex justify-center gap-6 mb-4">
+//         <Link href="https://github.com/CloudyZeeshan" target="_blank" rel="noopener noreferrer" className="w-10 h-10">
+//           <Image src="/github.png" alt="GitHub" className="w-full h-full" width={300} height={300}/>
+//         </Link>
+// {/*         <Link href="/" target="_blank" rel="noopener noreferrer" className="w-10 h-10"> */}
+//           <Image src="/linkedin.png" alt="LinkedIn" className="w-full h-full" width={300} height={300}/>
+//         </Link>
+// {/*         <Link href="/" target="_blank" rel="noopener noreferrer" className="w-10 h-10"> */}
+//           <Image src="/facebook.png" alt="Facebook" className="w-full h-full" width={300} height={300}/>
+//         </Link>
+//       </div>
+//       <div className="text-sm text-gray-500">&copy; {new Date().getFullYear()} Zeeshan Haider. All Rights Reserved.</div>
+//     </footer>
+//   );
+// };
+
+// export default Footer;
+
+
 import Image from "next/image";
 import Link from "next/link";
 
@@ -8,13 +34,7 @@ const Footer = () => {
       <div className="flex justify-center gap-6 mb-4">
         <Link href="https://github.com/CloudyZeeshan" target="_blank" rel="noopener noreferrer" className="w-10 h-10">
           <Image src="/github.png" alt="GitHub" className="w-full h-full" width={300} height={300}/>
-        </Link>
-{/*         <Link href="/" target="_blank" rel="noopener noreferrer" className="w-10 h-10"> */}
-          <Image src="/linkedin.png" alt="LinkedIn" className="w-full h-full" width={300} height={300}/>
-        </Link>
-{/*         <Link href="/" target="_blank" rel="noopener noreferrer" className="w-10 h-10"> */}
-          <Image src="/facebook.png" alt="Facebook" className="w-full h-full" width={300} height={300}/>
-        </Link>
+ 
       </div>
       <div className="text-sm text-gray-500">&copy; {new Date().getFullYear()} Zeeshan Haider. All Rights Reserved.</div>
     </footer>
@@ -25,6 +45,16 @@ export default Footer;
 
 
 
+
+// https://www.linkedin.com/in/dosto-ki-dunya-taimoor-207568317
+
+// https://www.facebook.com/taimoor.samiullahjan
+
+
+
+
+
+      
 
 // https://www.linkedin.com/in/dosto-ki-dunya-taimoor-207568317
 
